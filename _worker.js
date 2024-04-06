b0e07d4e6198408774c98d5c4949c1c1813923b8
JJC121 <166201753+JJC121@users.noreply.github.com>
2024-04-06 15:51:08 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,94 +1,99 @@
 
 // 部署完成后在网址后面加上这个，获取订阅器默认节点，/auto
 
-let mytoken= 'auto';//快速订阅访问入口, 留空则不启动快速订阅
+let mytoken= ['auto'];//快速订阅访问入口, 留空则不启动快速订阅
 
-// 设置优选地址，不带端口号默认443，不支持非TLS订阅生成
-//let addresses = [
-	//'icook.tw:2053#定义域名',
-	//'cloudflare.cfgo.cc#优选线路',
-
+// 设置优选地址，不带端口号默认443，TLS订阅生成
+let addresses = [
+	'icook.tw:2053#资源核优选域名',
+	//'cloudflare.cfgo.cc#优选官方线路',
 ];
 
 // 设置优选地址api接口
-//let addressesapi = [
-	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesapi.txt',
-	'',//可参考内容格式 自行搭建。
+let addressesapi = [
+	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesapi.txt', //可参考内容格式 自行搭建。
+	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesipv6api.txt', //IPv6优选内容格式 自行搭建。
+	'https://wenben-6kg.pages.dev/IP.txt?token=123456', 
 ];
 
-//设置优选地址，不带手口号默认80，noTLS订阅生成
-let addressesnotls =[
-	//'www.visa.com,sg#官方优选域名'，
-	//"ww,wt0,org:8080#官方优选域名'，
-	//"www,who.int:8880#官方优选域名')
-	'time.cloudflare.com:80#定义优选',
-'shopify.com:80#定义优选',
-'time.is:80#定义优选',
-'icook.hk:80#定义优选',
-'icook.tw:80#定义优选',
-'ip.sb:80#定义优选',
-'japan.com:80#定义优选',
-'malaysia.com:80#定义优选',
-'russia.com:80#定义优选',
-'singapore.com:80#定义优选',
-'skk.moe:80#定义优选',
-'www.visa.com:80#定义优选',
-'www.visa.com.sg:80#定义优选',
-'www.visa.com.hk:80#定义优选',
-'www.visa.com.tw:80#定义优选',
-'www.visa.co.jp:80#定义优选',
-'www.visakorea.com:80#定义优选',
-'www.gco.gov.qa:80#定义优选',
-'www.gov.se:80#定义优选',
-'www.gov.ua:80#定义优选',
-'www.digitalocean.com:80#定义优选',
-'www.csgo.com:80#定义优选',
-'www.shopify.com:80#定义优选',
-'www.whoer.net:80#定义优选',
-'www.whatismyip.com:80#定义优选',
-'www.ipget.net:80#定义优选',
-'www.hugedomains.com:80#定义优选',
-'www.udacity.com:80#定义优选',
-'www.4chan.org:80#定义优选',
-'www.okcupid.com:80#定义优选',
-'www.glassdoor.com:80#定义优选',
-'www.udemy.com:80#定义优选',
-'www.baipiao.eu.org:80#定义优选',
-'cdn.anycast.eu.org:80#定义优选',
-'cdn-all.xn--b6gac.eu.org:80#定义优选',
-'cdn-b100.xn--b6gac.eu.org:80#定义优选',
-'xn--b6gac.eu.org:80#定义优选',
-'edgetunnel.anycast.eu.org:80#定义优选',
-'alejandracaiccedo.com:80#定义优选',
-'nc.gocada.co:80#定义优选',
-'log.bpminecraft.com:80#定义优选',
-'www.boba88slot.com:80#定义优选',
-'gur.gov.ua:80#定义优选',
-'www.zsu.gov.ua:80#定义优选',
-'www.iakeys.com:80#定义优选',
-'edtunnel-dgp.pages.dev:80#定义优选',
-'www.d-555.com:80#定义优选',
-'fbi.gov:80#定义优选',
+// 设置优选地址，不带端口号默认80，noTLS订阅生成
+let addressesnotls = [
+	//'www.visa.com.sg#官方优选域名',
+	//'www.wto.org:8080#官方优选域名',
+	//'www.who.int:8880#官方优选域名',
+	'time.cloudflare.com:80#资源核优选',
+'shopify.com:80#资源核优选',
+'time.is:80#资源核优选',
+'icook.hk:80#资源核优选',
+'icook.tw:80#资源核优选',
+'ip.sb:80#资源核优选',
+'japan.com:80#资源核优选',
+'malaysia.com:80#资源核优选',
+'russia.com:80#资源核优选',
+'singapore.com:80#资源核优选',
+'skk.moe:80#资源核优选',
+'www.visa.com:80#资源核优选',
+'www.visa.com.sg:80#资源核优选',
+'www.visa.com.hk:80#资源核优选',
+'www.visa.com.tw:80#资源核优选',
+'www.visa.co.jp:80#资源核优选',
+'www.visakorea.com:80#资源核优选',
+'www.gco.gov.qa:80#资源核优选',
+'www.gov.se:80#资源核优选',
+'www.gov.ua:80#资源核优选',
+'www.digitalocean.com:80#资源核优选',
+'www.csgo.com:80#资源核优选',
+'www.shopify.com:80#资源核优选',
+'www.whoer.net:80#资源核优选',
+'www.whatismyip.com:80#资源核优选',
+'www.ipget.net:80#资源核优选',
+'www.hugedomains.com:80#资源核优选',
+'www.udacity.com:80#资源核优选',
+'www.4chan.org:80#资源核优选',
+'www.okcupid.com:80#资源核优选',
+'www.glassdoor.com:80#资源核优选',
+'www.udemy.com:80#资源核优选',
+'www.baipiao.eu.org:80#资源核优选',
+'cdn.anycast.eu.org:80#资源核优选',
+'cdn-all.xn--b6gac.eu.org:80#资源核优选',
+'cdn-b100.xn--b6gac.eu.org:80#资源核优选',
+'xn--b6gac.eu.org:80#资源核优选',
+'edgetunnel.anycast.eu.org:80#资源核优选',
+'alejandracaiccedo.com:80#资源核优选',
+'nc.gocada.co:80#资源核优选',
+'log.bpminecraft.com:80#资源核优选',
+'www.boba88slot.com:80#资源核优选',
+'gur.gov.ua:80#资源核优选',
+'www.zsu.gov.ua:80#资源核优选',
+'www.iakeys.com:80#资源核优选',
+'edtunnel-dgp.pages.dev:80#资源核优选',
+'www.d-555.com:80#资源核优选',
+'fbi.gov:80#资源核优选',
 ];
 
-let DLS = 4;//速度下限
+// 设置优选noTLS地址api接口
+let addressesnotlsapi = [
+	//'https://raw.githubusercontent.com/cmliu/CFcdnVmess2sub/main/addressesapi.txt', //可参考内容格式 自行搭建。
+];
+
+let DLS = 8;//速度下限
 let addressescsv = [
-	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressescsv.csv' //iptest测速结果文件。
+	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressescsv.csv', //iptest测速结果文件。
 ];
 
-let subconverter = "api.v1.mk"; //在线订阅转换后端，目前使用肥羊的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
-let subconfig = "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_Full_MultiMode.ini"; //订阅配置文件
-
+let subconverter = "apiurl.v1.mk"; //在线订阅转换后端，目前使用肥羊的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
+let subconfig = "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_Full_MultiMode.ini"; //订阅转换配置文件
+let noTLS = false; //改为 true , 将不做域名判断 始终返回noTLS节点
 let link = '';
 let edgetunnel = 'ed';
 let RproxyIP = 'false';
 let proxyIPs = [
-	//'proxyip.aliyun.fxxk.dedyn.io',
-	//'proxyip.multacom.fxxk.dedyn.io',
-	//'proxyip.vultr.fxxk.dedyn.io',
+	'proxyip.aliyun.fxxk.dedyn.io',
+	'proxyip.multacom.fxxk.dedyn.io',
+	'proxyip.vultr.fxxk.dedyn.io',
 ];
 let CMproxyIPs = [
-	{ proxyIP: "proxyip.fxxk.dedyn.io", type: "HK" },
+	//{ proxyIP: "proxyip.fxxk.dedyn.io", type: "HK" },
 ];
 let BotToken ='';
 let ChatID =''; 
@@ -103,7 +108,7 @@ let SUBUpdateTime = 6;
 let total = 99;//PB
 //let timestamp = now;
 let timestamp = 4102329600000;//2099-12-31
-
+const regex = /^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|\[.*\]):?(\d+)?#?(.*)?$/;
 async function sendMessage(type, ip, add_data = "") {
 	if ( BotToken !== '' && ChatID !== ''){
 		let msg = "";
@@ -127,14 +132,14 @@ async function sendMessage(type, ip, add_data = "") {
 	}
 }
 
-async function getAddressesapi() {
-	if (!addressesapi || addressesapi.length === 0) {
+async function getAddressesapi(api) {
+	if (!api || api.length === 0) {
 		return [];
 	}
 	
 	let newAddressesapi = [];
 	
-	for (const apiUrl of addressesapi) {
+	for (const apiUrl of api) {
 		try {
 			const response = await fetch(apiUrl);
 		
@@ -150,7 +155,7 @@ async function getAddressesapi() {
 			} else {
 				lines = text.split('\n');
 			}
-			const regex = /^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(:\d+)?(#.*)?$/;
+			//const regex = /^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(:\d+)?(#.*)?$/;
 		
 			const apiAddresses = lines.map(line => {
 				const match = line.match(regex);
@@ -167,7 +172,7 @@ async function getAddressesapi() {
 	return newAddressesapi;
 }
 
-async function getAddressescsv() {
+async function getAddressescsv(tls) {
 	if (!addressescsv || addressescsv.length === 0) {
 		return [];
 	}
@@ -210,7 +215,7 @@ async function getAddressescsv() {
 				const columns = lines[i].split(',');
 		
 				// 检查TLS是否为"TRUE"且速度大于DLS
-				if (columns[tlsIndex].toUpperCase() === 'TRUE' && parseFloat(columns[speedIndex]) > DLS) {
+				if (columns[tlsIndex].toUpperCase() === tls && parseFloat(columns[speedIndex]) > DLS) {
 					const ipAddress = columns[ipAddressIndex];
 					const port = columns[portIndex];
 					const dataCenter = columns[dataCenterIndex];
@@ -228,14 +233,26 @@ async function getAddressescsv() {
 	return newAddressescsv;
 }
 
+async function ADD(envadd) {
+	var addtext = envadd.replace(/[	 "'\r\n]+/g, ',').replace(/,+/g, ',');  // 将空格、双引号、单引号和换行符替换为逗号
+	//console.log(addtext);
+	if (addtext.charAt(0) == ',') addtext = addtext.slice(1);
+	if (addtext.charAt(addtext.length -1) == ',') addtext = addtext.slice(0, addtext.length - 1);
+	const add = addtext.split(',');
+	//console.log(add);
+	return add ;
+}
+
 let protocol;
 export default {
 	async fetch (request, env) {
-		mytoken = env.TOKEN || mytoken;
+		if (env.TOKEN) mytoken = await ADD(env.TOKEN);
+		//mytoken = env.TOKEN.split(',') || mytoken;
 		BotToken = env.TGTOKEN || BotToken;
 		ChatID = env.TGID || ChatID; 
 		subconverter = env.SUBAPI || subconverter;
 		subconfig = env.SUBCONFIG || subconfig;
+		FileName = env.SUBNAME || FileName;
 		const userAgentHeader = request.headers.get('User-Agent');
 		const userAgent = userAgentHeader ? userAgentHeader.toLowerCase() : "null";
 		const url = new URL(request.url);
@@ -247,12 +264,45 @@ export default {
 		total = total * 1099511627776 * 1024;
 		let expire= Math.floor(timestamp / 1000) ;
 
-		if (mytoken !== '' && url.pathname.includes(mytoken)) {
-			host = env.HOST || "edgetunnel-2z2.pages.dev";
-			uuid = env.UUID || "30e9c5c8-ed28-4cd9-b008-dc67277f8b02";
-			path = env.PATH || "/?ed=2048";
+		if (env.ADD) addresses = await ADD(env.ADD);
+		if (env.ADDAPI) addressesapi = await ADD(env.ADDAPI);
+		if (env.ADDNOTLS) addressesnotls = await ADD(env.ADDNOTLS);
+		if (env.ADDNOTLSAPI) addressesnotlsapi = await ADD(env.ADDNOTLSAPI);
+		if (env.ADDCSV) addressescsv = await ADD(env.ADDCSV);
+		DLS = env.DLS || DLS;
+
+		/*
+		console.log(`
+			addresses: ${addresses}
+			addressesapi: ${addressesapi}
+			addressesnotls: ${addressesnotls}
+			addressesnotlsapi: ${addressesnotlsapi}
+			addressescsv: ${addressescsv}
+			DLS: ${DLS}
+		`);
+		*/
+		
+		if (env.PROXYIP) proxyIPs = await ADD(env.PROXYIP);
+		//console.log(proxyIPs);
+
+		if (mytoken.length > 0 && mytoken.some(token => url.pathname.includes(token))) {
+			host = "null";
+			if (env.HOST) {
+				const hosts = await ADD(env.HOST);
+				host = hosts[Math.floor(Math.random() * hosts.length)];
+			}
+			uuid = env.UUID || "null";
+			path = env.PATH || "/?ed=2560";
 			edgetunnel = env.ED || edgetunnel;
 			RproxyIP = env.RPROXYIP || RproxyIP;
+
+			if (host == "null" || uuid == "null" ){
+				let 空字段;
+				if (host == "null" && uuid == "null") 空字段 = "HOST/UUID";
+				else if (host == "null") 空字段 = "HOST";
+				else if (uuid == "null") 空字段 = "UUID";
+				EndPS += ` 订阅器内置节点 ${空字段} 未设置！！！`;
+			}
 
 			const hasSos = url.searchParams.has('sos');
 			if (hasSos) {
@@ -265,7 +315,7 @@ export default {
 					}
 	
 					const base64Text = await subconverterResponse.text();
-					link = atob(base64Text); // 进行 Base64 解码
+					link += '\n' + atob(base64Text); // 进行 Base64 解码
 	
 				} catch (error) {
 					// 错误处理
@@ -326,16 +376,18 @@ export default {
 			}
 			
 			if (!path || path.trim() === '') {
-				path = '/?ed=2048';
+				path = '/?ed=2560';
 			} else {
 				// 如果第一个字符不是斜杠，则在前面添加一个斜杠
 				path = (path[0] === '/') ? path : '/' + path;
 			}
 		}
-
+		
+		noTLS = env.NOTLS || host.toLowerCase().includes('notls') || host.toLowerCase().includes('worker') || host.toLowerCase().includes('trycloudflare') || noTLS;
+		
 		if (userAgent.includes('telegram') || userAgent.includes('twitter') || userAgent.includes('miaoko')) {
 			return new Response('Hello World!');
-		} else if (userAgent.includes('clash') || (format === 'clash' && !userAgent.includes('subconverter'))) {
+		} else if ((userAgent.includes('clash') || (format === 'clash' && !userAgent.includes('subconverter'))) && !userAgent.includes('nekobox')) {
 			const subconverterUrl = `https://${subconverter}/sub?target=clash&url=${encodeURIComponent(request.url)}&insert=false&config=${encodeURIComponent(subconfig)}&emoji=true&list=false&tfo=false&scv=true&fdn=false&sort=false&new_name=true`;
 
 			try {
@@ -412,38 +464,118 @@ export default {
 				proxyhosts = [...new Set(proxyhosts)];
 			}
 			
-			const newAddressesapi = await getAddressesapi();
-			const newAddressescsv = await getAddressescsv();
+			const newAddressesapi = await getAddressesapi(addressesapi);
+			const newAddressescsv = await getAddressescsv('TRUE');
 			addresses = addresses.concat(newAddressesapi);
 			addresses = addresses.concat(newAddressescsv);
 			
 			// 使用Set对象去重
 			const uniqueAddresses = [...new Set(addresses)];
 			
+			let notlsresponseBody;
+			if(noTLS == true){
+				const newAddressesnotlsapi = await getAddressesapi(addressesnotlsapi);
+				const newAddressesnotlscsv = await getAddressescsv('FALSE');
+				addressesnotls = addressesnotls.concat(newAddressesnotlsapi);
+				addressesnotls = addressesnotls.concat(newAddressesnotlscsv);
+				const uniqueAddressesnotls = [...new Set(addressesnotls)];
+
+				notlsresponseBody = uniqueAddressesnotls.map(address => {
+					let port = "80";
+					let addressid = address;
+				
+					const match = addressid.match(regex);
+					if (!match) {
+						if (address.includes(':') && address.includes('#')) {
+							const parts = address.split(':');
+							address = parts[0];
+							const subParts = parts[1].split('#');
+							port = subParts[0];
+							addressid = subParts[1];
+						} else if (address.includes(':')) {
+							const parts = address.split(':');
+							address = parts[0];
+							port = parts[1];
+						} else if (address.includes('#')) {
+							const parts = address.split('#');
+							address = parts[0];
+							addressid = parts[1];
+						}
+					
+						if (addressid.includes(':')) {
+							addressid = addressid.split(':')[0];
+						}
+					} else {
+						address = match[1];
+						port = match[2] || port;
+						addressid = match[3] || address;
+					}
+	
+					//console.log(address, port, addressid);
+
+					if (edgetunnel.trim() === 'cmliu' && RproxyIP.trim() === 'true') {
+					// 将addressid转换为小写
+					let lowerAddressid = addressid.toLowerCase();
+					// 初始化找到的proxyIP为null
+					let foundProxyIP = null;
+						
+					// 遍历CMproxyIPs数组查找匹配项
+					for (let item of CMproxyIPs) {
+						if (lowerAddressid.includes(item.type.toLowerCase())) {
+							foundProxyIP = item.proxyIP;
+							break; // 找到匹配项，跳出循环
+						}
+					}
+						
+					if (foundProxyIP) {
+						// 如果找到匹配的proxyIP，赋值给path
+						path = `/proxyIP=${foundProxyIP}`;
+					} else {
+						// 如果没有找到匹配项，随机选择一个proxyIP
+						const randomProxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
+						path = `/proxyIP=${randomProxyIP}`;
+					}
+				}
+
+					const vlessLink = `vless://${uuid}@${address}:${port}?encryption=none&security=&type=ws&host=${host}&path=${encodeURIComponent(path)}#${encodeURIComponent(addressid)}`;
+			
+					return vlessLink;
+				}).join('\n');
+			}
+
 			const responseBody = uniqueAddresses.map(address => {
 				let port = "443";
 				let addressid = address;
 			
-				if (address.includes(':') && address.includes('#')) {
-					const parts = address.split(':');
-					address = parts[0];
-					const subParts = parts[1].split('#');
-					port = subParts[0];
-					addressid = subParts[1];
-				} else if (address.includes(':')) {
-					const parts = address.split(':');
-					address = parts[0];
-					port = parts[1];
-				} else if (address.includes('#')) {
-					const parts = address.split('#');
-					address = parts[0];
-					addressid = parts[1];
-				}
-			
-				if (addressid.includes(':')) {
-					addressid = addressid.split(':')[0];
-				}
+				const match = addressid.match(regex);
+				if (!match) {
+					if (address.includes(':') && address.includes('#')) {
+						const parts = address.split(':');
+						address = parts[0];
+						const subParts = parts[1].split('#');
+						port = subParts[0];
+						addressid = subParts[1];
+					} else if (address.includes(':')) {
+						const parts = address.split(':');
+						address = parts[0];
+						port = parts[1];
+					} else if (address.includes('#')) {
+						const parts = address.split('#');
+						address = parts[0];
+						addressid = parts[1];
+					}
 				
+					if (addressid.includes(':')) {
+						addressid = addressid.split(':')[0];
+					}
+				} else {
+					address = match[1];
+					port = match[2] || port;
+					addressid = match[3] || address;
+				}
+
+				//console.log(address, port, addressid);
+        
 				if (edgetunnel.trim() === 'cmliu' && RproxyIP.trim() === 'true') {
 					// 将addressid转换为小写
 					let lowerAddressid = addressid.toLowerCase();
@@ -471,7 +603,7 @@ export default {
 				let 伪装域名 = host ;
 				let 最终路径 = path ;
 				let 节点备注 = EndPS ;
-				if(proxyhosts && (host.includes('workers.dev') || host.includes('pages.dev'))) {
+				if(proxyhosts && (host.includes('.workers.dev') || host.includes('pages.dev'))) {
 					最终路径 = `/${host}${path}`;
 					伪装域名 = proxyhosts[Math.floor(Math.random() * proxyhosts.length)];
 					节点备注 = `${EndPS} 已启用临时域名中转服务，请尽快绑定自定义域！`;
@@ -481,9 +613,19 @@ export default {
 				return vlessLink;
 			}).join('\n');
 			
-			const combinedContent = responseBody + '\n' + link; // 合并内容
+			let combinedContent = responseBody; // 合并内容
+			
+			if (link) {
+				combinedContent += '\n' + link;
+				console.log("link: " + link)
+			}
+			
+			if (notlsresponseBody) {
+				combinedContent += '\n' + notlsresponseBody;
+				console.log("notlsresponseBody: " + notlsresponseBody);
+			}
+			
 			const base64Response = btoa(combinedContent); // 重新进行 Base64 编码
-
 
 			const response = new Response(base64Response, {
 				headers: { 
